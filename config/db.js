78bd80e2181515876523b441caaae3cac9e51c2c
davidhanenko <d.hanenko24@gmail.com>
2021-01-11 00:12:37 -0500

    Save

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+const config = require('config')
+const db = config.get('mongoURI')
+
+const connectDB = async () => {
+  try {
+    mongoose.connect(db, {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    })
+    console.log('MondoDB Connected')
+  } catch (err) {
+    console.error(err.message)
+    process.exit(1)
+  }
+}
+
+module.exports = connectDB
--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 const express = require('express')
+const connectDB = require('./config/db')
 
 const app = express()
+
+connectDB()
+
+// Init Middleware
+app.use(express.json({ extended: false }))
 
 app.use('/api/users', require('./routes/users'))
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/contacts', require('./routes/contacts'))
 
-
-app.get('/', (req,res) => {
-  res.json({msg:'Welcome to Contact Keeper API'})
+app.get('/', (req, res) => {
+  res.json({ msg: 'Welcome to Contact Keeper API' })
 })
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => console.log(`Server started on porn: ${PORT}`))
+app.listen(PORT, () => console.log(`Server started on port: ${PORT}`))
