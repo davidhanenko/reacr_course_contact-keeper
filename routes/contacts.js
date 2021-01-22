e500140a6cd22ed0a2b3d9a1e84a075dd29a1487
davidhanenko <d.hanenko24@gmail.com>
2021-01-22 02:52:47 -0500

    Try

--- routes/contacts.js
+++ routes/contacts.js
@@ -3,8 +3,8 @@ const router = express.Router()
 const auth = require('../middleware/auth')
 const { check, validationResult } = require('express-validator')
 
-const User = require('../models/User')
-const Contact = require('../models/Contact')
+const User = require('../models/user')
+const Contact = require('../models/contact')
 
 // @route     GET api/contacts
 // @desc      Get all users contacts
